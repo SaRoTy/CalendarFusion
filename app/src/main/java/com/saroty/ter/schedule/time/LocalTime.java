9d2e5e357063028fd217806e314f60a36431ecfb
Omen- <bellal.arthur@gmail.com>
2015-03-09 17:48:56 +0100

    Création d'un classe pour la gestion des heures.

---
+++ app/src/main/java/com/saroty/ter/schedule/time/LocalTime.java
@@ -0,0 +1,52 @@
+package com.saroty.ter.schedule.time;
+
+/**
+ * Created by Arthur on 09/03/2015.
+ */
+public class LocalTime
+{
+    private int hour;
+    private int minute;
+    private int second;
+
+    public LocalTime(int hour, int minute, int second)
+    {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public LocalTime(int hour, int minute)
+    {
+        this(hour, minute, 0);
+    }
+
+    public LocalTime(String rawLocalTime, String token)
+    {
+        String[] array = rawLocalTime.split(token);
+
+        if (array.length < 2 && array[0].matches("\\d+") && array[1].matches("\\d+")) //Check integer
+            return;
+
+        this.hour = Integer.parseInt(array[0]);
+        this.minute = Integer.parseInt(array[1]);
+
+        if (array.length > 2 && array[2].matches("\\d+"))
+            this.second = Integer.parseInt(array[2]);
+    }
+
+    public int getHour()
+    {
+        return hour;
+    }
+
+    public int getMinute()
+    {
+        return minute;
+    }
+
+    public int getSecond()
+    {
+        return second;
+    }
+}
